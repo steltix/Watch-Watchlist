0e61df21a3ef5ed36004c84f1ac6e155868aec98
Johan Teekens <johan@johanmbp2015>
2016-09-25 14:03:29 +0200

    Initial Commit

---
+++ Watch Watchlist WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Watch Watchlist WatchKit Extension
+//
+//  Created by Johan Teekens on 25/09/2016.
+//  Copyright © 2016 Steltix. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
