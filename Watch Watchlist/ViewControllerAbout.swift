b16d537465addaec835a7360479e21e5eec6746c
johanteekens <johan.teekens@steltix.com>
2016-10-07 15:57:01 +0200

    commit

--- Watch Watchlist/ViewControllerAbout.swift
+++ Watch Watchlist/ViewControllerAbout.swift
@@ -10,3 +10,23 @@ import Foundation
 import UIKit
 
 
+class ViewControllerAbout: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
